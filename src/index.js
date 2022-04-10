b64bdd8c3e935103955adf2737641c991c4d6da7
casasvoley <acasaszan@gmail.com>
2022-04-10 16:48:08 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,38 +17,6 @@ client.events = new Collection();
     require(`../handlers/${handler}`)(client, Discord);
 });
 
-// Get all the command files
-/* const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-for(const file of commandFiles){
-    const command = require(`../commands/${file}`);
-
-    client.commands.set(command.name, command);
-} */
-
-/* client.once('ready', () => {
-    console.log('Bot is online!');
-}); */
-
-// For each message
-client.on('messageCreate', message =>{
-    // If it does not start with the prefix or it is a message from the bot, ignore it
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-    // Remove the prefix and split by the spaces 
-    const args = message.content.slice(prefix.length).split(/ +/);
-    // Take elemnt 0 to lowercase
-    const command = args.shift().toLowerCase();
-
-    // Command behaviour
-    if(command === 'ping'){
-        client.commands.get('ping').execute(message, args, MessageEmbed);
-    } else if (command === 'help'){
-        client.commands.get('help').execute(message, args, MessageEmbed);
-    } else if (command === "turca"){
-        client.commands.get('turca').execute(message, args);
-    }
-});
-
 MongoClient.connect(env.MONGODB_SRV, function(err, client){
     console.log('Connected to the database!');
 });
