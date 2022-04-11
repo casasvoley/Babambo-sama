51cda371bd1e397849612b48cff156082ac46584
casasvoley <acasaszan@gmail.com>
2022-04-12 00:10:44 +0200

    Update queue.js

--- commands/queue.js
+++ commands/queue.js
@@ -11,25 +11,24 @@ module.exports = {
     execute(message, args, cmd, client, Discord) {
         const guildQueue = client.player.getQueue(message.guild.id);
 
-        if (!guildQueue) return message.channel.send(`No music currently playing ${message.author}... try again ? ❌`);
+        if (!guildQueue) return message.channel.send(`No se está reproduciendo música,  ${message.author}...  ❌`);
 
-        if (!guildQueue.songs[0]) return message.channel.send(`No music in the queue after the current one ${message.author}... try again ? ❌`);
+        if (!guildQueue.songs[0]) return message.channel.send(`No hay música en la cola ${message.author}...  ❌`);
 
         const embed = new MessageEmbed();
 
         embed.setColor('RED');
         embed.setThumbnail(message.guild.iconURL({ size: 2048, dynamic: true }));
-        embed.setAuthor({name: `Server queue - ${message.guild.name}`, iconURL: client.user.displayAvatarURL({ size: 1024, dynamic: true })});
+        embed.setAuthor({name: `Cola de reproducción`, iconURL: client.user.displayAvatarURL({ size: 1024, dynamic: true })});
 
         const songs = guildQueue.songs.map((song, i) => `**${i + 1}** - ${song.name} | ${song.author}`);
-
+        songs.shift();
         const numSongs = guildQueue.songs.length;
-        const nextSongs = numSongs > 5 ? `And **${numSongs - 5}** other song(s)...` : `In the playlist **${numSongs}** song(s)...`;
+        const nextSongs = numSongs > 5 ? `Y otra(s) **${numSongs - 5}** canción(es)...` : `En la cola hay **${numSongs}** canción(es)...`;
 
-        embed.setDescription(`Current ${guildQueue.nowPlaying.name}\n\n${songs.slice(0, 5).join('\n')}\n\n${nextSongs}`);
+        embed.setDescription(`Está sonando ${guildQueue.nowPlaying.name}\n\n${songs.slice(0, 5).join('\n')}\n\n${nextSongs}`);
 
         embed.setTimestamp();
-        embed.setFooter({text: 'Music comes first - Made with heart by Zerio ❤️', iconURL: message.author.avatarURL({ dynamic: true })});
 
         message.channel.send({ embeds: [embed] });
     },
