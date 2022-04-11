fd61b21d299eeff7b81428b6ffb30518d18c0513
casasvoley <acasaszan@gmail.com>
2022-04-11 23:45:25 +0200

    Queue añadido

---
+++ commands/queue.js
@@ -0,0 +1,35 @@
+// Librería Discord Music Player
+const dmp = require('discord-music-player');
+
+module.exports = {
+    name: 'queue',
+    aliases: ['q'],
+    cooldown: 0,
+    description: 'Muestra la cola de reproducción',
+
+    execute(message, args, cmd, client, Discord) {
+        const guildQueue = client.player.getQueue(message.guild.id);
+
+        if (!guildQueue) return message.channel.send(`No music currently playing ${message.author}... try again ? ❌`);
+
+        if (!guildQueue.songs[0]) return message.channel.send(`No music in the queue after the current one ${message.author}... try again ? ❌`);
+
+        const embed = new MessageEmbed();
+
+        embed.setColor('RED');
+        embed.setThumbnail(message.guild.iconURL({ size: 2048, dynamic: true }));
+        embed.setAuthor(`Server queue - ${message.guild.name}`, client.user.displayAvatarURL({ size: 1024, dynamic: true }));
+
+        const songs = guildQueue.songs.map((song, i) => `**${i + 1}** - ${song.name} | ${song.author}`);
+
+        const numSongs = guildQueue.songs.length;
+        const nextSongs = numSongs > 5 ? `And **${numSongs - 5}** other song(s)...` : `In the playlist **${numSongs}** song(s)...`;
+
+        embed.setDescription(`Current ${guildQueue.nowPlaying.name}\n\n${songs.slice(0, 5).join('\n')}\n\n${nextSongs}`);
+
+        embed.setTimestamp();
+        embed.setFooter('Music comes first - Made with heart by Zerio ❤️', message.author.avatarURL({ dynamic: true }));
+
+        message.channel.send({ embeds: [embed] });
+    },
+};
