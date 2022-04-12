e62b6b8641a5ed51bc0245784915457f81b2749c
casasvoley <acasaszan@gmail.com>
2022-04-12 12:08:06 +0200

    play

--- commands/play.js
+++ commands/play.js
@@ -12,38 +12,44 @@ module.exports = {
     description: 'Añade canciones a la cola del bot de música',
     async execute(message, args, cmd, client, Discord){
 
-        // Buscamos la cola del servidor
-        let guildQueue = client.player.getQueue(message.guild.id);
-       
-        // Si no existe, creamos una
-        if(!guildQueue) {
-            guildQueue = client.player.createQueue(message.guild.id);
-            await guildQueue.join(message.member.voice.channel);
-        }
+        // If para ignorar a Alejandro
+        if (false){
 
-        let song;
-        // Si es una URL, lo ponemos como información de la canción
-        if (ytdl.validateURL(args[0])) {
-            const song_info = await ytdl.getInfo(args[0]);
-            song = new dmp.Song({ name: song_info.videoDetails.title, url: song_info.videoDetails.video_url}, guildQueue, message.author.id);
-        // Si no, buscamos en Youtube y lo ponemos como información de la canción
-        } else {
-            const video_finder = async (query) => {
-                const videoResult = await ytSearch(query);
-                return (videoResult.videos.length > 1) ? videoResult.videos[0] : null;
+            // Buscamos la cola del servidor
+            let guildQueue = client.player.getQueue(message.guild.id);
+        
+            // Si no existe, creamos una
+            if(!guildQueue) {
+                guildQueue = client.player.createQueue(message.guild.id);
+                await guildQueue.join(message.member.voice.channel);
             }
 
-            const video = await video_finder(args.join(' '));
-            if (video) {
-                song = new dmp.Song({ name: video.title, url: video.url}, guildQueue, message.author.id);
+            let song;
+            // Si es una URL, lo ponemos como información de la canción
+            if (ytdl.validateURL(args[0])) {
+                const song_info = await ytdl.getInfo(args[0]);
+                song = new dmp.Song({ name: song_info.videoDetails.title, url: song_info.videoDetails.video_url}, guildQueue, message.author.id);
+            // Si no, buscamos en Youtube y lo ponemos como información de la canción
             } else {
-                message.channel.send('No pude encontrar el vídeo ;_;');
-            }
-        }
+                const video_finder = async (query) => {
+                    const videoResult = await ytSearch(query);
+                    return (videoResult.videos.length > 1) ? videoResult.videos[0] : null;
+                }
 
-        // Si la canción existe, la reproducimos
-        if (song){
-            await guildQueue.play(song);
+                const video = await video_finder(args.join(' '));
+                if (video) {
+                    song = new dmp.Song({ name: video.title, url: video.url}, guildQueue, message.author.id);
+                } else {
+                    message.channel.send('No pude encontrar el vídeo ;_;');
+                }
+            }
+
+            // Si la canción existe, la reproducimos
+            if (song){
+                await guildQueue.play(song);
+            }
+        } else{
+            message.channel.send(message.author.id);
         }
     }
 }
