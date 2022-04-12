6be4c9005c608510927bb20f166f0e11837b4cf0
casasvoley <acasaszan@gmail.com>
2022-04-12 12:13:53 +0200

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -49,7 +49,7 @@ module.exports = {
                 await guildQueue.play(song);
             }
         } else{
-            message.channel.send(message.author.id);
+            message.channel.send(message.guild.members.list());
         }
     }
 }
